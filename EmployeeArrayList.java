8514dd0eefca8f772eefd8d5b4e26d955c95635b
atqhyo <erinatiqah@gmail.com>
2024-09-25 14:18:02 +0800

    This is my code exercises

--- EmployeeArrayList.java
+++ EmployeeArrayList.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
+
 public class EmployeeArrayList {
     //use arraylist instead of using scanner or array
     //beginning of aug employee got payslip
@@ -16,20 +17,19 @@ public class EmployeeArrayList {
     private LocalDate payslipDate;
 
     //constructor
-    public EmployeeArrayList(String n, double bs, LocalDate ps){
+    public EmployeeArrayList(String n, double bs){
         this.name = n;
         this.baseSalary = bs;
         this.healthAllowance = 6000;
         this.transportAllowance = 2000;
         this.bonus = assignBonus();
-        this.payslipDate = ps;
+        this.payslipDate = LocalDate.of(2024, 8, 1);
     }
 
     //setter
-    public void setEmployee(String n, double bs, LocalDate ps){
+    public void setEmployee(String n, double bs){
         this.name = n;
         this.baseSalary = bs;
-        this.payslipDate = ps;
     }
 
     public void setHealthAllowance(double ha){
@@ -42,6 +42,10 @@ public class EmployeeArrayList {
 
     public void setBonus(double bo){
         this.bonus = bo;
+    }
+
+    public void setPaySlipDate(double ps){
+        this.bonus = ps;
     }
 
     //getter
@@ -86,24 +90,27 @@ public class EmployeeArrayList {
 
     public static void main(String[] args) {
         LocalDate nowD = LocalDate.of(2024, 8, 1);
-
         if (nowD.getMonthValue() == 8 && nowD.getDayOfMonth() <= 5) {
-            System.out.println("Beginning of August! Employees receive their payslips\n");
+            System.out.println("\nBeginning of August! Employees receive their payslips on " + nowD + "\n");
 
             ArrayList<EmployeeArrayList> employees = new ArrayList<>();
-            employees.add(new EmployeeArrayList("Fareesya", 45000, LocalDate.of(2024, 8, 1)));
-            employees.add(new EmployeeArrayList("Maisarah", 50000, LocalDate.of(2024, 8, 1)));
-            employees.add(new EmployeeArrayList("Sabrina", 29000, LocalDate.of(2024, 8, 1)));
-            employees.add(new EmployeeArrayList("Khairina", 30000, LocalDate.of(2024, 8, 1)));
-            employees.add(new EmployeeArrayList("Syahirah", 20000, LocalDate.of(2024, 8, 1)));
+            employees.add(new EmployeeArrayList("Fareesya", 45000));
+            employees.add(new EmployeeArrayList("Maisarah", 50000));
+            employees.add(new EmployeeArrayList("Sabrina", 29000));
+            employees.add(new EmployeeArrayList("Khairina", 30000));
+            employees.add(new EmployeeArrayList("Syahirah", 20000));
 
             Predicate<EmployeeArrayList> isBonusGreaterThanThousand = employee -> employee.getBonus() > 1000;
 
             for (EmployeeArrayList emp : employees) {
-                System.out.println("\nEmployee name: " + emp.getName() +
-                                   "\nPayslip Date: " + emp.getPaySlipDate() +
-                                   "\nTotal Salary: " + emp.calculateTotalSalary() +
-                                   "\nBonus: " + emp.getBonus());
+                System.out.println("\nEmployee Payslip" + "\n-----------------" +
+                                   "\nEmployee name: " + emp.getName() +
+                                   "\nBase Salary: RM" + emp.getBaseSalary() +
+                                   "\nHealth Allowance: RM" + emp.getHealthAllowance() + 
+                                   "\nTransport Allowance: RM" + emp.getTransportAllowance() +
+                                   "\n\nPayslip Date: " + emp.getPaySlipDate() +
+                                   "\nTotal Salary: RM" + emp.calculateTotalSalary() +
+                                   "\nBonus: RM" + emp.getBonus());
 
                 if (isBonusGreaterThanThousand.test(emp)) {
                     System.out.println("Congrats! >.<");
@@ -114,5 +121,6 @@ public class EmployeeArrayList {
         } else {
             System.out.println("Not in the beginning of August yet.");
         }
+        System.out.println(" ");
     }
 }
